c1a667d8fbbececda8492053d9da00ad85869c8e
RuiRomano <ruiromano@microsoft.com>
2025-03-23 17:45:57 +0100

    prd scenario

--- README.md
+++ README.md
@@ -31,30 +31,29 @@ Before running the Github Action, ensure you configure the following [GitHub act
 ## Topology
 
 ```mermaid
-flowchart LR
-
-    subgraph sources                
-        data_prd[(Files)]
-        data_dev[(Files)]
-    end
+flowchart TB
 
     subgraph GitHub
-        direction LR
-        feature[feature branch]:::branch
-        main[main branch]:::branch
-        build[build pipeline - BPA rules]:::gh_workflow
-        deploy[deploy pipeline]:::gh_workflow        
+        direction TB
+        feature[feature branch]:::gh_branch
+        main[main branch]:::gh_branch
+        build[build pipeline - BPA rules]:::gh_action
+        deploy[deploy pipeline]:::gh_action        
     end
 
+    data_dev[(Files - Dev)]
+
     subgraph Dev
-        direction LR
-        subgraph workspace_dev[dev workspace]
-            datapipeline_dev
-            notebook_dev
-            lakehouse_dev
-            semanticmodel_dev
-            reports_dev
+        direction TB
+        
+        subgraph workspace_dev[Fabric dev workspace]
+            datapipeline_dev[data pipeline]
+            notebook_dev[notebook]
+            lakehouse_dev[lakehouse]
+            semanticmodel_dev[semantic model]
+            reports_dev[reports]
         end     
+
         data_dev-->datapipeline_dev
         datapipeline_dev-->lakehouse_dev
         notebook_dev-->lakehouse_dev
@@ -62,16 +61,19 @@ flowchart LR
         lakehouse_dev-->semanticmodel_dev
     end
 
+    data_prd[(Files - Prod)]
+
     subgraph Prod
         direction LR
-        subgraph workspace_data[data workspace]
-            datapipeline_prd
-            notebook_prd
-            lakehouse_prd         
+        
+        subgraph workspace_data[Fabric data workspace]
+            datapipeline_prd[data pipeline]
+            notebook_prd[notebook]
+            lakehouse_prd[lakehouse] 
         end
-        subgraph workspace_analytics[analytics workspace]
-            semanticmodel_prd
-            reports_prd
+        subgraph workspace_analytics[Fabric analytics workspace]
+            semanticmodel_prd[semantic model]
+            reports_prd[reports]
         end        
                 
         data_prd-->datapipeline_prd
@@ -82,9 +84,11 @@ flowchart LR
         lakehouse_prd---semanticmodel_prd
     end
     
-
     feature-- PR --->build-- merge --> main --> deploy --> Dev
     
     deploy-- schedule every day --->Prod
+
+    classDef gh_action fill: #1E90FF, color:white;
+    classDef gh_branch fill:#e8b63e;    
 
 ```
--- scripts/deploy-prd.py
+++ scripts/deploy-prd.py
@@ -1,5 +1,3 @@
-##### WORK IN PROGRESS #####
-
 import os
 import time
 import argparse
@@ -21,13 +19,14 @@ capacity_name = args.capacity
 workspace_name = args.workspace
 admin_upns = args.admin_upns
 
-workspace_name += "-PRD"
+workspace_name_data = f"{workspace_name} - Data"
+workspace_name_analytics = f"{workspace_name} - Analytics"
 
 if admin_upns:
     admin_upns = [upn.strip() for upn in admin_upns.split(",")]
 
 lakehouse_name = "LH_STORE_RAW"
-connection_name = "SalesSense - DEV"
+connection_name = "SalesSense - PRD"
 connection_source_url = "https://raw.githubusercontent.com/pbi-tools/sales-sample/refs/heads/data/RAW-Sales.csv"
 
 # Authenticate
@@ -46,14 +45,16 @@ connection_id = create_connection(
     }    
 )
 
-# Create workspace
+# Data Workspace
 
-workspace_id = create_workspace(workspace_name, capacity_name, upns=admin_upns)
+workspace_id_data = create_workspace(workspace_name_data, capacity_name, upns=admin_upns)
+
+workspace_id_analytics = create_workspace(workspace_name_analytics, capacity_name, upns=admin_upns)
 
 # Create lakehouse
 
 lakehouse_id = create_item(
-    workspace_name=workspace_name,
+    workspace_name=workspace_name_data,
     item_type="lakehouse",
     item_name=lakehouse_name,
     parameters={"enableSchemas": "true"},
@@ -63,12 +64,12 @@ lakehouse_id = create_item(
 
 deploy_item(
     "src/DP_INGST_CopyCSV.DataPipeline",
-    workspace_name=workspace_name,
+    workspace_name=workspace_name_data,
     find_and_replace={
         (
             r"pipeline-content.json",
             r'("workspaceId"\s*:\s*)".*"',
-        ): rf'\1"{workspace_id}"',
+        ): rf'\1"{workspace_id_data}"',
         (
             r"pipeline-content.json",
             r'("artifactId"\s*:\s*)".*"',
@@ -84,7 +85,7 @@ deploy_item(
 
 deploy_item(
     "src/NB_TRNSF_Raw.Notebook",
-    workspace_name=workspace_name,
+    workspace_name=workspace_name_data,
     find_and_replace={
         (
             r"notebook-content.ipynb",
@@ -97,7 +98,7 @@ deploy_item(
         (
             r"notebook-content.ipynb",
             r'("default_lakehouse_workspace_id"\s*:\s*)".*"',
-        ): rf'\1"{workspace_id}"',
+        ): rf'\1"{workspace_id_data}"',
         (
             r"notebook-content.ipynb",
             r'("known_lakehouses"\s*:\s*)\[[\s\S]*?\]',
@@ -110,7 +111,7 @@ deploy_item(
 for attempt in range(3):
 
     sql_endpoint = run_fab_command(
-        f"get /{workspace_name}.workspace/{lakehouse_name}.lakehouse -q properties.sqlEndpointProperties.connectionString",
+        f"get /{workspace_name_data}.workspace/{lakehouse_name}.lakehouse -q properties.sqlEndpointProperties.connectionString",
         capture_output=True,
     )
 
@@ -123,7 +124,7 @@ if not sql_endpoint:
 
 semanticmodel_id = deploy_item(
     "src/SM_SalesSense.SemanticModel",
-    workspace_name=workspace_name,
+    workspace_name=workspace_name_analytics,
     find_and_replace={
         (
             r"expressions.tmdl",
@@ -138,7 +139,7 @@ for report_path in glob.glob("src/*.Report"):
 
     deploy_item(
         report_path,
-        workspace_name=workspace_name,
+        workspace_name=workspace_name_analytics,
         find_and_replace={
             ("definition.pbir", r"\{[\s\S]*\}"): json.dumps(
                 {
